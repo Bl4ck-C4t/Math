eb43dc7e5daf6b3819f33d0f76f7859c00ddb3a3
TheShadow88 <stef4oben88@gmail.com>
2016-09-26 14:30:39 +0300

    Calculates D
    A bit improved

--- disc.py
+++ disc.py
@@ -1,9 +1,12 @@
-import math
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-c = int(input("Enter c: "))
-D = pow(b,2) - (4*(a*c))
-x1 = (-b + math.sqrt(D))/(2*a)
-x2 = (-b - math.sqrt(D))/(2*a)
-print("x1 = {}\nx2 = {}".format(x1,x2))
-input("Press any key to exit...")
+while True:
+    import math
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    c = int(input("Enter c: "))
+    D = pow(b,2) - (4*(a*c))
+    x1 = (-b + math.sqrt(D))/(2*a)
+    x2 = (-b - math.sqrt(D))/(2*a)
+    print("x1 = {}\nx2 = {}".format(x1,x2))
+    cb = input("Press 'e' to exit, any key to reset...")
+    if cb == "e":
+        exit()
