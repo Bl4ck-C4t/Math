65f76f45509a4cbf2f067425f8c6632764c313f8
TheShadow88 <stef4oben88@gmail.com>
2016-09-26 14:41:11 +0300

    done

--- disc.py
+++ disc.py
@@ -4,9 +4,15 @@ while True:
     b = int(input("Enter b: "))
     c = int(input("Enter c: "))
     D = pow(b,2) - (4*(a*c))
-    x1 = (-b + math.sqrt(D))/(2*a)
-    x2 = (-b - math.sqrt(D))/(2*a)
-    print("x1 = {}\nx2 = {}".format(x1,x2))
+    if D < 0:
+        print("No sulution D = {}".format(D))
+    elif D == 0:
+        x = (-b + math.sqrt(D))/(2*a)
+        print("x = {}".format(x))
+    else:
+        x1 = (-b + math.sqrt(D))/(2*a)
+        x2 = (-b - math.sqrt(D))/(2*a)
+        print("x1 = {}\nx2 = {}".format(x1,x2))
     cb = input("Press 'e' to exit, any key to reset...")
     if cb == "e":
         exit()
